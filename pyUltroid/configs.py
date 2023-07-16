c376c8f9ee0174050ea0086a70fd81855da82352
ashan2006 <128839543+ashan2006@users.noreply.github.com>
2023-07-16 15:00:01 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -20,21 +20,22 @@ except ImportError:
 class Var:
     # mandatory
     API_ID = (
-        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=6, cast=int)
+        int(sys.argv[1]) if len(sys.argv) > 1 else config("API_ID", default=16267784, cast=int)
     )
     API_HASH = (
         sys.argv[2]
         if len(sys.argv) > 2
-        else config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+        else config("API_HASH", default="b29707434744713a3dad070bda43d856")
     )
-    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
+    SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=1BVtsOMQBu2-xXRWaEJKAeVZu6u1i46dQoPAGzKvrr4pZ0D8x3WWzvthKzm2CrTD5ntXqopnHh5VQ4e1BN9ajteb62z0y8OcWT97v6z2v1Cyl0rvD-OkO9BSE28FMG8fkuxFsycNSfg0wosdXQ-gBWeRN5HtB9ao-nC-IBQP6Sdj0hS8Mhb7ma1nGodNFCpyIQ-DU017-1qbXt5cvzzD73VTuJEc1Kya95N7WVgbOxghh9DXT_H8qoQZkvuRhF1WVb7t8PHVbcARPYlQ_5Gc2dUqwzuu6bW_o8evBHNSuvXLrBVTrBQyp2ZR0ivQj8JIj6DlQ0Km9X9DIxuaH-6F1NTxPQHMkhfU=)
     REDIS_URI = (
         sys.argv[4]
         if len(sys.argv) > 4
-        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=None))
+        else (config("REDIS_URI", default=None) or config("REDIS_URL", default=redis-14108.c13.us-east-1-3.ec2.cloud.redislabs.com:14108))
     )
     REDIS_PASSWORD = (
-        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=None)
+        sys.argv[5] if len(sys.argv) > 5 else config("REDIS_PASSWORD", default=iD7sjrQ2xNtbmfxGcfnq5WkRWWspyUTe
+)
     )
     # extras
     BOT_TOKEN = config("BOT_TOKEN", default=None)
